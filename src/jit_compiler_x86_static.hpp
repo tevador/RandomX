529b933fcf997c3de83f102197f94a4cf8491813
cryptonote-social <cryptonote.social@gmail.com>
2021-01-23 13:02:54 -0800

    remove unnecessary first-load initialization code

--- src/jit_compiler_x86_static.hpp
+++ src/jit_compiler_x86_static.hpp
@@ -32,7 +32,6 @@ extern "C" {
 	void randomx_prefetch_scratchpad();
 	void randomx_prefetch_scratchpad_end();
 	void randomx_program_prologue();
-	void randomx_program_prologue_first_load();
 	void randomx_program_loop_begin();
 	void randomx_program_loop_load();
 	void randomx_program_start();
