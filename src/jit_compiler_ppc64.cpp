tevador/RandomX

commit 8cd6435064dd677256ad064e5daa4791dd6710bf

Optimize Group E register conversion on PPC64
With the Group E AND mask properly set to include just the bits we want
to keep from the original register, we can combine the `vand` and `vor`
operations into a single `vsel` operation, saving us one vector
instruction every time we need to load a Group E register from memory.

This gives us an extra ~1.3% boost to performance on both V1 and V2.

src/jit_compiler_ppc64.cpp
@@ -309,6 +309,7 @@ namespace PPC64 {
 	}
 
 	static inline uint32_t vperm(uint32_t vrt, uint32_t vra, uint32_t vrb, uint32_t vrc) { return VA_form(4, vrt, vra, vrb, vrc, 43); }
+	static inline uint32_t vsel(uint32_t vrt, uint32_t vra, uint32_t vrb, uint32_t vrc) { return VA_form(4, vrt, vra, vrb, vrc, 42); }
 
 	static inline uint32_t vand(uint32_t vrt, uint32_t vra, uint32_t vrb) { return VX_form(4, vrt, vra, vrb, 1028); }
 	static inline uint32_t vor(uint32_t vrt, uint32_t vra, uint32_t vrb) { return VX_form(4, vrt, vra, vrb, 1156); }
@@ -1311,8 +1312,7 @@ namespace randomx {
 		uint32_t tmp_gpr = jit->getTempGpr();
 		uint32_t tmp_vr = jit->getTempVr();
 		emitLoadVsrFromScratchpad(state, tmp_gpr, tmp_vr, isn);
-		state.emit(PPC64::vand(tmp_vr, tmp_vr, ConstantVectorGroupEAndMaskVR17));
-		state.emit(PPC64::vor(tmp_vr, tmp_vr, ConstantVectorGroupEOrMaskVR19));
+		state.emit(PPC64::vsel(tmp_vr, ConstantVectorGroupEOrMaskVR19, tmp_vr, ConstantVectorGroupEAndMaskVR17));
 		state.emit(PPC64::xvdivdp(dst, dst, 32 + tmp_vr));
 	}
 	static void h_FSQRT_R(HANDLER_ARGS) {

src/jit_compiler_ppc64_static.S
@@ -714,14 +714,10 @@ randomx_ppc64_vm_loop_prologue:
 	xvcvsxwdp %vs37, %vs37
 	xvcvsxwdp %vs38, %vs38
 	xvcvsxwdp %vs39, %vs39
-	vand %v4, %v4, %v17
-	vand %v5, %v5, %v17
-	vand %v6, %v6, %v17
-	vand %v7, %v7, %v17
-	vor %v4, %v4, %v19
-	vor %v5, %v5, %v19
-	vor %v6, %v6, %v19
-	vor %v7, %v7, %v19
+	vsel %v4, %v19, %v4, %v17
+	vsel %v5, %v19, %v5, %v17
+	vsel %v6, %v19, %v6, %v17
+	vsel %v7, %v19, %v7, %v17
 
 randomx_ppc64_vm_loop_prologue_end:
 

commit 694dd0043ea81888f3b03520ae4fde18ca0dbf15

PPC64 JIT: Correct maximum RandomX instruction code size
Fixes: 8cd6435 ("Optimize Group E register conversion on PPC64")

src/jit_compiler_ppc64.cpp
@@ -530,7 +530,7 @@ namespace randomx {
 	static const size_t ReciprocalPoolPos = sizeConstants + 16;  // Add 16 bytes for the Group E OR vector mask
 	static const size_t ConstantPoolSize = alignSize(sizeConstants + 16 + ReciprocalPoolSize, CodeAlign);  // Add 16 bytes for the Group E OR vector mask
 	static const size_t ReserveCodeSize = alignSize(sizeVmPrologue + sizeVmEpilogue + sizeVmLoopPrologue + sizeVmDataRead + sizeVmDataReadLight + sizeVmSpadStorePrologue + sizeVmSpadStoreMixV2SoftAes + sizeVmSpadStoreEpilogue, CodeAlign);
-	constexpr size_t MaxRandomXInstrCodeSize = 4*10;  // FDIV_M requires at most 10 instructions
+	constexpr size_t MaxRandomXInstrCodeSize = 4*9;  // FDIV_M and CFROUND require at most 9 instructions
 	constexpr size_t MaxSuperscalarInstrSize = 4*6;  // IMUL_RCP requires at most 6 instructions
 	static const size_t SuperscalarProgramHeaders = sizeSshashSingleItemPrologue + sizeSshashSingleItemEpilogue;
 