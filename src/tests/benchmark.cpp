40e40cfbcb4042f5c0ee8a4316f278fb99de71e4
SChernykh <15806605+SChernykh@users.noreply.github.com>
2026-05-09 17:13:02 +0200

    Prepare v2.0.1 release

--- src/tests/benchmark.cpp
+++ src/tests/benchmark.cpp
@@ -182,7 +182,7 @@ int main(int argc, char** argv) {
 
 	store32(&seed, seedValue);
 
-	std::cout << "RandomX benchmark v2.0" << std::endl;
+	std::cout << "RandomX benchmark v2.0.1" << std::endl;
 
 	if (help) {
 		printUsage(argv[0]);
